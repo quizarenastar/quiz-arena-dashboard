0a52aa411690a18b3afbb19d67d3676f4aa22fd6
muskan khatoon <sheikhmuskan2227@gmail.com>
2025-09-04 12:23:15 +0530

    Initialize Quiz-Arena Dashboard project with Vite, React, and Tailwind CSS; set up basic app structure and configuration files.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div>
+            <h1 class="text-3xl font-bold underline">Quiz-Arena Dashboard</h1>
+        </div>
+    );
+};
+
+export default App;
