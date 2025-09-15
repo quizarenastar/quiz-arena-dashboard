6afbebfafab9463b8aea84d4649e02050c03cb90
mohdrafey1 <mohdrafey600@gmail.com>
2025-09-15 15:27:31 +0530

    refactor: comment out Home route in RoutesComponent

--- src/RoutesComponent.jsx
+++ src/RoutesComponent.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+// import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 
 const RoutesComponent = () => {
     return (
         <Routes>
-            <Route path='/' element={<Home />} />
+            {/* <Route path='/' element={<Home />} /> */}
             <Route path='/login' element={<Login />} />
             <Route path='/signup' element={<SignUp />} />
         </Routes>
