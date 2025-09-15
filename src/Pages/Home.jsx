9cef64e5701294fd30a33ed1eb3a1eb301f6a8e1
muskan khatoon <sheikhmuskan2227@gmail.com>
2025-09-15 16:39:13 +0530

    feat: update index.html title and implement StatsCard component in Home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import StatsCard from '../Components/StatsCard';
 
 const Home = () => {
     return (
-        <>
-            <div>Home</div>
-        </>
+        <div className='container mx-auto'>
+            <StatsCard />
+        </div>
     );
 };
+
 export default Home;
