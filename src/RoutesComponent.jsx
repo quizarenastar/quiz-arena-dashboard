0df2f035c7acbb02117092b0e0d25485b0c69f42
mohdrafey1 <mohdrafey600@gmail.com>
2025-09-15 15:29:29 +0530

    refactor: comment out Login and Sign Up routes in RoutesComponent

--- src/RoutesComponent.jsx
+++ src/RoutesComponent.jsx
@@ -7,8 +7,8 @@ const RoutesComponent = () => {
     return (
         <Routes>
             {/* <Route path='/' element={<Home />} /> */}
-            <Route path='/login' element={<Login />} />
-            <Route path='/signup' element={<SignUp />} />
+            {/* <Route path='/login' element={<Login />} />
+            <Route path='/signup' element={<SignUp />} /> */}
         </Routes>
     );
 };
